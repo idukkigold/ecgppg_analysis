b3ae59ec12e6868c7cba6d01470a6f89427ea9c5
idukkigold <nithish.nitt@gmail.com>
2018-11-04 13:48:43 -0500

    Updated p.py

--- p.py
+++ p.py
@@ -1,1 +1,19 @@
-import heartbeat
+import heartbeat as hb
+import numpy as np
+import matplotlib.pyplot as plt
+
+rfile =open("5-gamers/gamer1-ppg-2000-01-01.csv",'r')
+ppg=[]
+for line in rfile:
+    line=line.split(',')
+    ppg.append(line[1].strip("\n"))
+    # print(line)
+
+
+
+plt.plot(ppg)
+plt.show()
+
+# hr= hb.get_data(ppg)
+
+# print(hr)
